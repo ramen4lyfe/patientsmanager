b1ce5eecd37e3f70b95b2ad37b7b71daeb8cf8f7
Quan Do <quando@quans-mac-mini.lan>
2022-08-07 23:26:25 -0700

    finihsed create form

--- client/src/components/CreatePatient.js
+++ client/src/components/CreatePatient.js
@@ -28,8 +28,79 @@ const CreatePatient = () => {
     };
 
   return (
-    <div>CreatePatient</div>
+    <div className="container ">
+        <div className="row">
+            <div className="mt-2">
+                <h3>Add a new patient</h3>
+                <form onSubmit={handleSubmit} className="row">
+                    <div className="form-group col-6 mb-4 mt-2">
+                        <label className="mb-2" htmlFor="firstName">First Name: </label>
+                        <input 
+                            type="text"
+                            className="form-control mb-4"
+                            onChange={(e) => setFirstName(e.target.value)}
+                            value={firstName}
+                        />
+                        {errors.name ? <p className="text-danger">{errors.firstName.message}</p> : null}
+                        
+                        <label className="mb-2" htmlFor="lastName">Last Name: </label>
+                        <input 
+                            type="text"
+                            className="form-control mb-4"
+                            onChange={(e) => setLasttName(e.target.value)}
+                            value={lastName}
+                        />
+                        {errors.type ? <p className="text-danger">{errors.lastName.message}</p> : null}
+
+                        <label className="mb-2" htmlFor="DOB">Date of Birth: </label>
+                        <input 
+                            type="textarea"
+                            className="form-control mb-4"
+                            onChange={(e) => setDOB(e.target.value)}
+                            value={DOB}
+                        />
+                        {errors.description ? <p className="text-danger">{errors.DOB.message}</p> : null}
+                    </div>
+                    <div className="form-group col-6 mb-4 mt-2">
+                        <label className="mb-2" htmlFor="age">Age: </label>
+                            <input 
+                                type="text"
+                                className="form-control mb-4"
+                                onChange={(e) => setAge(e.target.value)}
+                                value={age}
+                            />
+
+                        <label className="mb-2" htmlFor="appointmentType">Appointment Type: </label>
+                            <input 
+                                type="text"
+                                className="form-control mb-4"
+                                onChange={(e) => setAppointmentType(e.target.value)}
+                                value={appointmentType}
+                            />
+
+                        <label className="mb-2" htmlFor="discussionTopic">Discussion Topic: </label>
+                            <input 
+                                type="text"
+                                className="form-control mb-4"
+                                onChange={(e) => setDiscussionTopic(e.target.value)}
+                                value={discussionTopic}
+                            />
+
+                        <label className="mb-2" htmlFor="summary">Summary: </label>
+                            <input 
+                                type="text"
+                                className="form-control mb-4"
+                                onChange={(e) => setSummary(e.target.value)}
+                                value={summary}
+                            />
+                    </div>
+                    <button type="submit" className="btn btn-primary btn-block mb-2">Create Patient</button>
+                    <Link to="/api/patient/list" className="btn btn-secondary btn-block">Cancel</Link>
+                </form>
+            </div>
+        </div>
+    </div>
   )
 }
 
-export default CreatePatient
+export default CreatePatient;
