67f7b6dd48c2172e25a799ffb51bc1ff8a9131ef
ramen4lyfe <do.quan.m@gmail.com>
2022-08-11 02:49:41 -0700

    Trying to create shortlist

--- client/src/components/ShortList.js
+++ client/src/components/ShortList.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { useState,useEffect } from 'react';
 import axios from "axios";
+import {Link, useParams, useNavigate} from 'react-router-dom';
+import Card from "react-bootstrap/Card";
+import ListGroup from 'react-bootstrap/ListGroup';
 
 
 const ShortList = () => {
@@ -16,8 +19,19 @@ const ShortList = () => {
       });
   }, []);
 
+
+  const listItems = patientData.map((patient) =>
+   <Card style={{ width: 'auto' }} key={patient._id}>
+   <Card.Header>{patient.firstName} {patient.lastName}</Card.Header>
+   <ListGroup variant="flush">
+     <ListGroup.Item>Age: {patient.age}</ListGroup.Item>
+     <ListGroup.Item>first name: {patient.summary}</ListGroup.Item>
+   </ListGroup>
+ </Card>
+);
   return (
-    <div>ShortList You mom</div>
+    // <div>ShortList You mom</div>
+    <ul>{listItems}</ul>
   )
 }
 
--- client/src/components/data/ShortListData.js
+++ client/src/components/data/ShortListData.js
@@ -21,23 +21,30 @@ import axios from "axios";
 //     });
 // };
 
-const ShortListData = (idFromBelow) => {
-    const [patientData, setPatientData] = useState([]);
-    useEffect(() => {
-        axios.get(`http://localhost:8000/api/patient/${idFromBelow}`)
-        .then((response) => {
-            console.log("selected for short list");
-            console.log(response);
+// const ViewPatient = (props) => {
+//     const {id} = useParams();
+//     const [patient, setPatient] = useState({});
+//     const navigate = useNavigate(); 
+  
+//     // for modal
+//     const [show, setShow] = useState(false);
+//     const handleClose = () => setShow(false);
+//     const handleShow = () => setShow(true);
+  
+//     useEffect(() => {
+//         axios.get(`http://localhost:8000/api/patient/details/${id}`)
+//           .then((res) => {
+//               console.log(res);
+//               console.log(res.data);
+//               setPatient(res.data);
+//           })
+//           .catch((err)=>console.log(err));
+//       }, [id])
+  
+      
+    };
+return (
 
-            const selectedPatient = patientData.filter((patient) => {
-                return patient._id !== idFromBelow;
-            });
-            setPatientData(selectedPatient);
-        })
-        .catch((err) => {
-            console.log("Error adding to short list", err.response);
-        });
-    });
-};
+);
 
 export default ShortListData;
