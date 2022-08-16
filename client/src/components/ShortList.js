a0b88a9736adce12e19d2811ae421327273cbbe9
ramen4lyfe <do.quan.m@gmail.com>
2022-08-16 01:39:05 -0700

    implementing shortlist

--- client/src/components/ShortList.js
+++ client/src/components/ShortList.js
@@ -4,6 +4,7 @@ import axios from "axios";
 import {Link, useParams, useNavigate} from 'react-router-dom';
 import Card from "react-bootstrap/Card";
 import ListGroup from 'react-bootstrap/ListGroup';
+import moment from 'moment';
 // import ShortListData from "./data/ShortListData";
 import PatientList from './PatientList';
 
@@ -25,28 +26,33 @@ const ShortList = (props) => {
   const idList = patientData.map(patient => {
     // const name = patientData[id].name
     console.log('hi',patient.firstName)
+    return(
+      // <li key={patient._id}>{patient.firstName}</li>
+      console.log()
+    )
   })
 
   
 return (
   <div className="container-fluid-md d-md-flex justify-content-center align-item-center">
     <h2>Short List</h2>
-    {/* <div className="p-2">
-      <h4>Details about: {patient.firstName} {patient.lastName}</h4>
+     <div className="p-2">
+      {idList}
+      {/* <h4>Details about: {idList.firstName} {idList.lastName}</h4>
       <Card style={{ width: 'auto' }}>
-        <Card.Header>{patient.firstName} {patient.lastName}</Card.Header>
+        <Card.Header>{idList.firstName} {idList.lastName}</Card.Header>
         <ListGroup variant="flush">
-          <ListGroup.Item>Gender: {patient.gender}</ListGroup.Item>
-          <ListGroup.Item>Age: {patient.age}</ListGroup.Item>
-          <ListGroup.Item>DOB: {moment(patient.DOB).format('MMMM Do, YYYY')}</ListGroup.Item>
-          <ListGroup.Item>Appointment Type: {patient.appointmentType}</ListGroup.Item>
-          <ListGroup.Item>Discussion Topic: {patient.discussionTopic}</ListGroup.Item>
-          <ListGroup.Item>Summary: {patient.summary}</ListGroup.Item>
+          <ListGroup.Item>Gender: {idList.gender}</ListGroup.Item>
+          <ListGroup.Item>Age: {idList.age}</ListGroup.Item>
+          <ListGroup.Item>DOB: {moment(idList.DOB).format('MMMM Do, YYYY')}</ListGroup.Item>
+          <ListGroup.Item>Appointment Type: {idList.appointmentType}</ListGroup.Item>
+          <ListGroup.Item>Discussion Topic: {idList.discussionTopic}</ListGroup.Item>
+          <ListGroup.Item>Summary: {idList.summary}</ListGroup.Item>
         </ListGroup>
       </Card>
-      <Link to={`/api/patient/update/${patient._id}`} className="btn btn-warning m-2">Update Record</Link>
-      <Button variant="danger" onClick={handleShow}>Delete Record</Button>
-    </div> */}
+      <Link to={`/api/patient/update/${idList._id}`} className="btn btn-warning m-2">Update Record</Link> */}
+      {/* <Button variant="danger" onClick={handleShow}>Delete Record</Button> */}
+    </div> 
   </div>
   )
 };
