2f5dd46c469e25ebc706ebd0d5fdc2cd2bc4992a
ramen4lyfe <do.quan.m@gmail.com>
2022-09-03 15:23:57 -0700

    edit nav bar

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,20 +1,24 @@
 import Nav from 'react-bootstrap/Nav';
 import { NavLink } from 'react-router-dom';
+import CreateModal from './modals/CreateModal';
 
 
 function NavTab() {
     return (
-            <Nav justify variant="tabs">
+        <div className="d-flex justify-content-between">
+            <Nav  variant="tabs">
                 <Nav.Item>
                     <Nav.Link as={NavLink} to={"/api/patient/shortlist"}>Short List</Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
                     <Nav.Link as={NavLink} to={"/api/patient/list"}>Patients List</Nav.Link>
                 </Nav.Item>
-                <Nav.Item>
+                {/* <Nav.Item>
                     <Nav.Link as={NavLink} to={"/api/patient/new"}>Add New Patient</Nav.Link>
-                </Nav.Item>
+                </Nav.Item> */}
             </Nav>
+            <CreateModal />
+        </div>
     );
 }
 
