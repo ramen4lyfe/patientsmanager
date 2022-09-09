614cee606b4ffe0c6b6737bd186188efc4b6d38c
ramen4lyfe <do.quan.m@gmail.com>
2022-09-08 17:51:51 -0700

    updateModal

--- client/src/components/modals/UpdateModal.js
+++ client/src/components/modals/UpdateModal.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import {React, useState} from 'react'
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import CreateForm from '../forms/CreateForm';
+import CreatePatient from '../CreatePatient';
+import axios from 'axios';
+import UpdatePatient from '../UpdatePatient';
 
-const UpdateModal = () => {
-  return (
-    <div>UpdateModal</div>
-  )
-}
+
+function UpdateModal () {
+
+
+
+    return (
+        <>
+        <Button variant="outline-primary" onClick={handleShow}>
+        + Add New Patient
+        </Button>
+
+        <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+        >
+        <Modal.Header closeButton>
+            <Modal.Title>Add New Patient</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            <UpdatePatient />
+        </Modal.Body>
+        <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+            Close
+            </Button>
+            <Button variant="primary" onClick={handleSubmit}>Add Patient</Button>
+        </Modal.Footer>
+        </Modal>
+    </>
+    )
+    }
 
 export default UpdateModal
