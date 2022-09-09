f081925ae01700e5c86598b5252679a7fee93727
ramen4lyfe <do.quan.m@gmail.com>
2022-09-08 18:01:05 -0700

    add functionality tp update Modal

--- client/src/components/modals/UpdateModal.js
+++ client/src/components/modals/UpdateModal.js
@@ -1,10 +1,10 @@
-import {React, useState} from 'react'
+import {React, useState, useEffect} from 'react'
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
-import CreateForm from '../forms/CreateForm';
-import CreatePatient from '../CreatePatient';
 import axios from 'axios';
 import UpdatePatient from '../UpdatePatient';
+import { useParams, useNavigate, Link } from 'react-router-dom';
+
 
 
 function UpdateModal () {
@@ -12,11 +12,66 @@ function UpdateModal () {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
+  const {id} = useParams();
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLasttName] = useState("");
+  const [gender, setGender] = useState("");
+  const [DOB, setDOB] = useState("");
+  const [age, setAge] = useState("");
+  const [appointmentType, setAppointmentType] = useState("");
+  const [discussionTopic, setDiscussionTopic] = useState("");
+  const [summary, setSummary] = useState("");
+//   const navigate = useNavigate();
+  const [errors, setErrors] = useState({});
+
+  useEffect(() => {
+    axios
+        .get(`http://localhost:8000/api/patient/details/${id}`)
+        .then((response) => {
+            // console.log(response.data);
+            setFirstName(response.data.firstName);
+            setLasttName(response.data.lastName);
+            setGender(response.data.gender);
+            setDOB(response.data.DOB);
+            setAge(response.data.age);
+            setAppointmentType(response.data.appointmentType);
+            setDiscussionTopic(response.data.discussionTopic);
+            setSummary(response.data.summary);
+        })
+        .catch((err) => {
+            console.log(err.response.data.error.errors);
+            // setErrors(err.response.data.error.errors);
+        });
+}, [id]);
+
+  const handleSubmit = (e) => {
+  e.preventDefault();
+  axios
+  .put(`http://localhost:8000/api/patient/update/${id}`, {
+    firstName, 
+    lastName, 
+    gender, 
+    DOB, 
+    age, 
+    appointmentType, 
+    discussionTopic, 
+    summary,  
+  })
+  .then((response) => {
+      console.log(response);
+    //   navigate("/api/patient/list");
+  })
+  .catch((err) => {
+      console.log(err.response.data.error.errors);
+      setErrors(err.response.data.error.errors);
+  });
+};
+
 
     return (
         <>
-        <Button variant="outline-primary" onClick={handleShow}>
-        + Add New Patient
+        <Button onClick={handleShow} className="btn btn-warning btn-sm m-1">
+        Update
         </Button>
 
         <Modal
@@ -26,7 +81,7 @@ function UpdateModal () {
         keyboard={false}
         >
         <Modal.Header closeButton>
-            <Modal.Title>Add New Patient</Modal.Title>
+            <Modal.Title>Update Record</Modal.Title>
         </Modal.Header>
         <Modal.Body>
             <UpdatePatient />
@@ -35,7 +90,7 @@ function UpdateModal () {
             <Button variant="secondary" onClick={handleClose}>
             Close
             </Button>
-            {/* <Button variant="primary" onClick={handleSubmit}>Add Patient</Button> */}
+            <Button variant="primary" onClick={handleSubmit}>Update</Button>
         </Modal.Footer>
         </Modal>
     </>
