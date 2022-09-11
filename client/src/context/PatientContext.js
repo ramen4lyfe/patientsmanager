a0149463ef4e885a45a8c02d7328f6b71193bb37
ramen4lyfe <do.quan.m@gmail.com>
2022-09-10 17:18:59 -0700

    improvements

--- client/src/context/PatientContext.js
+++ client/src/context/PatientContext.js
@@ -3,8 +3,23 @@ import axios from "axios";
 
 export const PatientContext = createContext()
 
-// const PatientList = () => {
-//     const [patientData, setPatientData] = useState([]);
+const PatientInfo = (props) => {
+    const [patientData, setPatientData] = useState()
+
+    useEffect(() => {
+        axios.get("http://localhost:8000/api/patient")
+        .then((response) => {
+            console.log(response.data);
+            setPatientData(response.data);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    }, []);
+
+
+// export function PatientProvider({props}) {
+//     const [patientData, setPatientData] = useState();
 //     useEffect(() => {
 //       axios.get("http://localhost:8000/api/patient")
 //         .then((response) => {
@@ -15,30 +30,22 @@ export const PatientContext = createContext()
 //             console.log(err);
 //         });
 //     }, []);
-// };
 
-export function PatientProvider({props}) {
-    const [patientData, setPatientData] = useState([]);
-    useEffect(() => {
-      axios.get("http://localhost:8000/api/patient")
-        .then((response) => {
-            console.log(response.data);
-            setPatientData(response.data);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-    }, []);
-
-    const value = patientData.map((patient) => 
-    <ul>
-    <li>{patient._id}</li>
-    <li>{patient.firstName}</li>
-    </ul>
-    )
+    // const value = patientData.map((patient) => 
+    // <ul>
+    //     <li>{patient._id}</li>
+    //     <li>{patient.firstName}</li>
+    //     <li>{patient.lastName}</li>
+    //     <li>{patient.gender}</li>
+    //     <li>{patient.dob}</li>
+    //     <li>{patient.age}</li>
+    //     <li>{patient.appointmentType}</li>
+    //     <li>{patient.discussionTopic}</li>
+    // </ul>
+    // )
 
     return (
-        <PatientContext.Provider value={{ patientData, setPatientData }}>
+        <PatientContext.Provider value={{ patientData }}>
             {props.children}
         </PatientContext.Provider>
     );
